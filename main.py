47c0696594869e158df620047fb49a1b2a8eb8e1
dinorchan <1094173715@qq.com>
2023-03-25 13:51:43 +0800

    update

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    uvicorn.run('run:app', host="0.0.0.0", port=8000, reload=True, debug=True, workers=1)
+    uvicorn.run('run:app', host="175.24.205.189", port=8000, reload=True, workers=1)
 
 
 
